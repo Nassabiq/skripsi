d8b16cb7a588f9cbb2fdb8e4c2b001648e921d3f
Nassabiq <nasirudinsabiq@gmail.com>
2022-11-11 07:35:45 +0700

    11111

--- api/database/seeders/UserSeeder.php
+++ api/database/seeders/UserSeeder.php
@@ -17,11 +17,11 @@ class UserSeeder extends Seeder
     public function run()
     {
         $role = ['Manager Inventory', 'Manager Penjualan', 'Manager IT', 'Operator Inventory', 'Operator Produksi', 'Operator Finishing', 'Marketing', 'Pelanggan'];
-        $id_user = IdGenerator::generate(['table' => 'users', 'field' => 'id_user', 'length' => 10, 'prefix' => 'USER-']);
+        // $id_user = IdGenerator::generate(['table' => 'users', 'field' => 'id_user', 'length' => 10, 'prefix' => 'user-']);
 
         foreach ($role as $data) {
             $user = User::create([
-                'id_user' => $id_user,
+                'id_user' => IdGenerator::generate(['table' => 'users', 'field' => 'id_user', 'length' => 10, 'prefix' => 'user-']),
                 'name' => $data,
                 'email' => Str::slug($data) . '@app.com',
                 'password' => bcrypt('user1234'),
